eb20fb3ac8955abc64a8f835a7c4b85c70246f08
Bonga Ngcobo <Bonga.Developer@outlook.com>
2018-10-30 01:42:13 +0200

    adding the files

---
+++ SerapisPatientAPI/Controllers/PracticeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SerapisPatientAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Practice")]
+    public class PracticeController : ControllerBase
+    {
+        // GET: api/Practice
+        [HttpGet]
+        public void Get()
+        {
+            //return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Practice/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+        
+        // POST: api/Practice
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+        
+        // PUT: api/Practice/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ SerapisPatientAPI/Model/Practice.cs
@@ -0,0 +1,73 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SerapisPatientAPI.Model
+{
+    public class Practice
+    {
+        [BsonId]
+        //[BsonRepresentation(BsonType.ObjectId)]
+        public ObjectId Id { get; set; }
+
+        [BsonElement("practiceName")]
+        public string PracticeName { get; set; }
+
+        [BsonElement("practicePicture")]
+        public string PracticePicture { get; set; }
+
+        [BsonElement("location")]
+        public Location Location { get; set; }
+
+        [BsonElement("DoctorAvaliable")]
+        public List<Doc> DoctorAvaliable { get; set; }
+    }
+    public partial class Doc
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        //[BsonId]
+        public ObjectId docid { get; set; }
+    }
+    public partial class Location
+    {
+        [BsonElement("latitude")]
+        public string Latitude { get; set; }
+
+        [BsonElement("longitude")]
+        public string Longitude { get; set; }
+
+        [BsonElement("workAddress")]
+        public WorkAddress WorkAddress { get; set; }
+
+    }
+
+        //public partial class Id
+        //{
+
+        //    [BsonRepresentation(BsonType.ObjectId)]
+        //    [BsonId]
+        //    public string Doctorid { get; set; }
+
+        //}
+
+    public partial class WorkAddress
+    {
+
+        [BsonElement("addressLine1")]
+        public string AddressLine1 { get; set; }
+
+        [BsonElement("addressLine2")]
+        public string AddressLine2 { get; set; }
+
+        [BsonElement("townOrCity")]
+        public string TownOrCity { get; set; }
+
+        [BsonElement("postal")]
+        public int Postal { get; set; } //might be a long type !=int
+
+    }
+    
+}
